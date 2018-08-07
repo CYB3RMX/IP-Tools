715e72ebbf4e1c3f328c38c3b01601bbedbe9cc0
CYB3RMX <42123683+CYB3RMX@users.noreply.github.com>
2018-08-07 19:46:24 +0300

    Add files via upload

--- IPTools.py
+++ IPTools.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-import os
+import os,socket
 print("\u001b[33mIPTools by CYB3RMX_")
 print("\u001b[37m///////////////////")
 print("[1] PING")
@@ -8,6 +8,10 @@ print("[2] NETSTAT")
 print("[3] WHOIS")
 print("[4] NSLookup")
 print("[5] ARP")
+print("[6] DOMAIN TO IP ADDRESS")
+print("[7] TRACEROUTE")
+print("[8] NMAP")
+print("[99] UPDATE IP-Tools")
 print("///////////////////")
 select = int(input("\u001b[33mCHOOSE: "))
 if select == 1:
@@ -96,5 +100,63 @@ elif select == 5:
         os.system('python IPTools.py')
     else:
         print("OK.")
+elif select == 6:
+    os.system('clear')
+    print("\u001b[36mDOMAIN TO IP ADDRESS")
+    print("××××××××××××××××××××××××××××××")
+    domain = str(input("WRITE TARGET DOMAIN: "))
+    print(socket.gethostbyname(domain))
+    print("××××××××××××××××××××××××××××××")
+    ret6 = int(input("RETURN BACK TO MAIN MENU [1/0]?: "))
+    if ret6 == 1:
+        os.system('clear')
+        os.system('python IPTools.py')
+elif select == 7:
+    os.system('clear')
+    print("\u001b[35mTRACEROUTE")
+    print("€€€€€€€€€€€€€€€€€€€€")
+    trace = str(input("WRITE TARGET DOMAIN: "))
+    os.system('traceroute '+trace)
+    print("€€€€€€€€€€€€€€€€€€€€")
+    ret7 = int(input("RETURN BACK TO MAIN MENU [1/0]?: "))
+    if ret7 == 1:
+        os.system('clear')
+        os.system('python IPTools.py')
+elif select ==8:
+    os.system('clear')
+    print("\u001b[32mNMAP NETWORK ANALYSIS")
+    print("oooooooooooooooooooooooooooo")
+    print("[1] ALIVE HOST SCAN")
+    print("[2] SIMPLE SCAN")
+    print("[3] DETAILED SCAN")
+    print("[0] RETURN BACK TO MAIN MENU")
+    print("oooooooooooooooooooooooooooo")
+    nmapsel = int(input("CHOOSE: "))
+    if nmapsel == 1:
+        os.system('nmap 192.168.1.0/24')
+        ret8 = int(input("RETURN BACK TO MAIN MENU [1/0]?: "))
+        if ret8 == 1:
+            os.system('clear')
+            os.system('python IPTools.py')
+    elif nmapsel == 2:
+        scan = str(input("WRITE TARGET: "))
+        os.system('nmap '+scan)
+        ret9 = int(input("RETURN BACK TO MAIN MENU [1/0]?: "))
+        if ret9 == 1:
+            os.system('clear')
+            os.system('python IPTools.py')
+    elif nmapsel == 3:
+        os.system('nmap 192.168.1.0/24 -sV -vv')
+        ret10 = int(input("RETURN BACK TO MAIN MENU [1/0]?: "))
+        if ret10 == 1:
+            os.system('clear')
+            os.system('python IPTools.py')
+    else:
+        os.system('clear')
+        os.system('python IPTools.py')
+elif select == 99:
+    print("\u001b[32m[*] UPDATING IP-Tools...")
+    os.system('git clone https://github.com/CYB3RMX/IP-Tools')
+    print("[*] UPDATE COMPLETE.")
 else:
     print("\u001b[31mYOU SELECTED WRONG OPTION!!!")
